cd160b9a6a8a879d576504ed29eebc33d9e9f5f1
Monika-sharma88 <59170983+Monika-sharma88@users.noreply.github.com>
2019-12-28 12:12:45 +0530

    Updates

---
+++ precache-manifest.395096dd83403d1154ca290b32d2dcca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e950c475a351633adf1875a40864c1",
+    "url": "/contactmanger/index.html"
+  },
+  {
+    "revision": "672825022f4974782a82",
+    "url": "/contactmanger/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "5d93a986c8a3e3f58c62",
+    "url": "/contactmanger/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "672825022f4974782a82",
+    "url": "/contactmanger/static/js/2.859c3807.chunk.js"
+  },
+  {
+    "revision": "5d93a986c8a3e3f58c62",
+    "url": "/contactmanger/static/js/main.1631fc60.chunk.js"
+  },
+  {
+    "revision": "1d03b90df8c87eef079f",
+    "url": "/contactmanger/static/js/runtime-main.cc3eecd9.js"
+  }
+]);
